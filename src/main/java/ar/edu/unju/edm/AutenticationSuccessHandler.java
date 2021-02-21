6f5a1269472c3c069656b22c27c1bb716b0eb89e
Lucas <Lucas@DESKTOP-HUN66VH>
2021-02-21 17:54:01 -0300

    Implementacion del security

---
+++ src/main/java/ar/edu/unju/edm/AutenticationSuccessHandler.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.edm;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutenticationSuccessHandler implements AuthenticationSuccessHandler{
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+	
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		boolean tipoAdmin = false;
+		boolean tipoCliente = false;
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		for(GrantedAuthority  grantedAuthority : authorities) {
+			if(grantedAuthority.getAuthority().equals("Admin")) {
+				tipoAdmin = true;
+				break;
+			}else if (grantedAuthority.getAuthority().equals("Cliente")){
+				tipoCliente = true;
+				break;
+			}
+		}
+		if(tipoAdmin) {
+			redirectStrategy.sendRedirect(request, response, "/Admin");
+		}else if(tipoCliente) {
+			redirectStrategy.sendRedirect(request, response, "/Cliente");
+		}else {
+			throw new IllegalStateException();
+		}	
+	}
+}
---
+++ src/main/java/ar/edu/unju/edm/service/imp/LoginUsuarioServiceImp.java
@@ -0,0 +1,35 @@
+package ar.edu.unju.edm.service.imp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unju.edm.model.Usuario;
+import ar.edu.unju.edm.repository.IUsuarioDAO;
+
+@Service
+public class LoginUsuarioServiceImp implements UserDetailsService {
+
+	@Autowired
+	IUsuarioDAO iUsuarioDAO;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuarioEncontrado = iUsuarioDAO.findByUser(username).orElseThrow(()-> new UsernameNotFoundException("Login Invalido"));
+		List<GrantedAuthority> tipos = new ArrayList<>();
+		GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(usuarioEncontrado.getTipo());
+		tipos.add(grantedAuthority);
+		UserDetails usuario = (UserDetails) new User(username,usuarioEncontrado.getPassword(),tipos);
+		System.out.println("USUARIO:  "+usuarioEncontrado.getUser()+"CONTRASEÑA:  "+usuarioEncontrado.getPassword()+"  TIPO:  "+usuarioEncontrado.getTipo());
+		return usuario;
+	}
+
+}
